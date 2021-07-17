73f98cfc0f98b13ff200f26c6d0ac8f12f754fe5
sujitha-msit <sujicmc@msitprogram.net>
2021-07-17 16:41:42 +0530

    done

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -6,7 +6,7 @@
 
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	area=0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))
-	print(area)
+	return area
 triangleareabycoordinates(23, 30, 15, 15, 50, 25)
 # , 222.50)
 triangleareabycoordinates(23, 30, 15, 15, 32, 15)
