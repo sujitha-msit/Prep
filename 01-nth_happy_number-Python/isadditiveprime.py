44b4df0eb3fa04c9b43c7f83b78820f4b58a70e9
sujitha-msit <sujicmc@msitprogram.net>
2021-07-23 17:59:35 +0530

    done

--- 01-nth_happy_number-Python/isadditiveprime.py
+++ 01-nth_happy_number-Python/isadditiveprime.py
@@ -7,7 +7,7 @@ def prime(number):
         if number%i==0:
             return False
     return True
-def isadditive(number):
+def isAdditivePrime(number):
     if prime(number):
         if(len(str(number))!=1):
             return isadditive(sum(map(int,list(str(number)))))
@@ -16,4 +16,17 @@ def isadditive(number):
     else:
         print("not prime",number)
         return False
-print(isadditive(31))
+print(isAdditivePrime(31))
+assert (isAdditivePrime(2) == True)
+assert (isAdditivePrime(3) == True)
+assert (isAdditivePrime(5) == True)
+assert (isAdditivePrime(13) == False)
+assert (isAdditivePrime(23) == True)
+assert (isAdditivePrime(29) == True)
+assert (isAdditivePrime(41) == True)
+assert (isAdditivePrime(98) == False)
+assert (isAdditivePrime(198) == False)
+assert (isAdditivePrime(290) == False)
+assert (isAdditivePrime(67) == True)
+assert (isAdditivePrime(97) == False)
+print("All test cases passed... :-)")
