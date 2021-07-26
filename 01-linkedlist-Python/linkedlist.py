6d7a3037763a573c00c87f8f206563fc66948ff3
sujitha-msit <sujicmc@msitprogram.net>
2021-07-26 13:00:33 +0530

    done

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -19,14 +19,28 @@ class LinkedList(object):
         
     def append(self, new_element):
         # Your code goes here
-        pass
+        temp=self.head
+        if self.head==None:
+            self.head=new_element
+        while(temp.next!=None):
+            temp=temp.next
+        temp.next=new_element
+        
             
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
-        pass
+        temp=self.head
+        if self.head==None:
+            return None
+        while(position!=1):
+            temp=temp.next
+            position-=1
+            if temp==None:
+                return None
+        return temp
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
@@ -34,10 +48,56 @@ class LinkedList(object):
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
+        temp=self.head
+        
+        if position==1:
+            head=new_element
+            new_element.next=temp
+        while(position!=1):
+            temp=temp.next
+            position-=1
+        t=temp.next
+        temp.next=new_element
+        new_element.next=t
     
     
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
-        pass
+        temp=self.head.next
+        prv=self.head
+        found=False
+        if self.head==None:
+            return None
+        if prv.value==value:
+            self.head=self.head.next
+        while(not found):
+            if temp==None:
+                return "Not found"
+            if temp.value==value :
+                found=True
+                prv.next=temp.next
+            else:
+                prv=prv.next
+                temp=temp.next
+    def printing(self):
+        temp=self.head
+        while(temp!=None):
+            print(temp.value)
+            temp=temp.next
+
+
+# e1 = Element(1)
+# e2 = Element(2)
+# e3 = Element(3)
+
+# ll = LinkedList(e1)
+# ll.append(e2)
+# ll.append(e3)
+# # ll.delet0e(2)
+# # print(ll.get_position(1))
+# # print(ll.get_position(2))
+# e4 = Element(4)
+# ll.insert(e4,3)
+# ll.delete(1)
+# ll.printing()
