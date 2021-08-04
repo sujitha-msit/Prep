fd7b673fe8536ae76b728c0ed5a259a7fc1b5463
sujitha-msit <sujicmc@msitprogram.net>
2021-08-04 18:34:02 +0530

    done

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -9,4 +9,47 @@
 
 
 def fun_nth_smithnumber(n):
-    return 1
+   
+    number=4
+    i=0
+    while(True):
+        if smith(number)==True:
+            i+=1
+        if i==n:
+            break
+        number=number+1
+    return number
+def smith(number):
+    a=[]
+    original=number
+    for i in range(2,(number//2)+1):
+        if number%i==0:    
+            if prime(i):
+                
+                a.append(i)
+    count=0
+    # print(a)
+    for i in a:
+        while(number%i==0):
+            number=number//i
+            if len(str(i))>1:
+                temp=sum(map(int,list(str(i))))
+                count+=temp
+            else:
+                count+=i
+    if sum(map(int,list(str(original))))==count:
+        return True
+    else:
+        return False
+def prime(number):
+    if number==2:
+        return True
+    if number%2==0:
+        return False
+    for i in range(3,int(number**0.5)+1):
+        if number%i==0:
+            return False
+    return True
+# print(smith(22))
+for i in range(5):
+    print(fun_nth_smithnumber(i))
