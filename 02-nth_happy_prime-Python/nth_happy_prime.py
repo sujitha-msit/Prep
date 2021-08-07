e37a938c22ea6acb3fc7f153861b66c41d288ed6
sujitha-msit <sujicmc@msitprogram.net>
2021-08-07 18:53:45 +0530

    dome

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -5,4 +5,33 @@
 
 
 def fun_nth_happy_prime(n):
-	return 0
+	i=0
+	number=2
+	while(True):
+		if ishappy(number) and isprime(number):
+			i=i+1
+			print(number)
+		if i==n:
+			break
+		number=number+1
+	return number
+		
+
+def ishappy(number):
+	l=map(int,list(str(number)))
+	l=map(square,l)
+	if sum(l)==1:
+		return True
+	return False
+def square(x):
+	return x*x
+def isprime(number):
+	if number==2:
+		return True
+	if number%2==0:
+		return False
+	for i in range(3,int(number**0.5),2):
+		if number%i==0:
+			return False
+	return True
+print(ishappy(7))
