e616dc4b044b2d2ef902b008fd8ba7378d94ca77
sujitha-msit <sujicmc@msitprogram.net>
2021-08-07 18:11:52 +0530

    done

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -6,4 +6,29 @@
 
 def nthwithproperty309(n):
 	# Your code goes here
-	pass
+	number=309
+	i=0
+	while(True):
+		if property309(number):
+			i=i+1
+			print(number)
+		if i==n:
+			break
+		number=number+1
+	return number
+
+def property309(number):
+	for i in range(10):
+		if str(i) not in str(number**5):
+			return False
+	return True
+
+# number=309
+# if "0" and "1" and "2" and "3" and "4" and "5" and "6" and "7" and "8" and "9" in str(number**5):
+
+# 	print(True)
+# else:
+# 	print(False)
+# print(property309(309
+# ))
+print(nthwithproperty309(1000))
